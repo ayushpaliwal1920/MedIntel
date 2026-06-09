42e2f3ad0d7d4c9f5d35cc70ff27f30c248d0174
Ayush Paliwal <ayushpaliwal1920@gmail.com>
2026-06-09 23:42:49 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages ,setup
+
+setup(
+    name = "MedIntel_chatbot",
+    version = "0.1.0",
+    author = "AyushP",
+    author_email="ayushpaliwal1922@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
